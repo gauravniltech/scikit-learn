cad000e13f025c2353089c926384b6ddee89473a
Gaurav <gks.com131@gmail.com>
2020-05-04 15:12:47 +0530

    fixed the import

--- sklearn/svm/__init__.py
+++ sklearn/svm/__init__.py
@@ -11,7 +11,7 @@ The :mod:`sklearn.svm` module includes Support Vector Machine algorithms.
 # License: BSD 3 clause (C) INRIA 2010
 
 from ._classes import SVC, NuSVC, SVR, NuSVR, OneClassSVM, LinearSVC, \
-        LinearSVR
+        LinearSVR, SVDD
 from ._bounds import l1_min_c
 
 __all__ = ['LinearSVC',
@@ -21,4 +21,5 @@ __all__ = ['LinearSVC',
            'OneClassSVM',
            'SVC',
            'SVR',
-           'l1_min_c']
+           'l1_min_c',
+           'SVDD']
